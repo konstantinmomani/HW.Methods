443294b925e78f67090a480b3d316063e7b863f1
konstantin <mom.konstantin@gmail.com>
2023-02-09 04:27:38 +0300

    task 1-2

--- src/com/homework/Methods.java
+++ src/com/homework/Methods.java
@@ -1,10 +1,71 @@
 package com.homework;
 
-public class Methods {
-    public static void main (String[] args) {
-        task1();
-    }
-    public static void task1() {
+import java.time.LocalDate;
 
+public class Methods {
+
+    public static boolean isItleapYear(int year) {
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
+
+    public static String deviceOS(int os) {
+        String osName;
+        if (os == 0) {
+            osName = "iOS";
+        } else {
+            osName = "Android";
+        }
+        return osName;
+    }
+
+    public static String deviceYear(int year) {
+        String osType;
+        if (year == 2023) {
+            osType = "стандартную";
+        } else {
+            osType = "облегченную";
+        }
+        return osType;
+    }
+
+    public static void main(String[] args) {
+
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача1");
+        int year = 2021;
+        if (isItleapYear(year)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача2");
+        int clientOS = 0;
+        int clientDeviceYear = 2023;
+        String osName = deviceOS(clientOS);
+        String osType = deviceYear(clientDeviceYear);
+        System.out.println("Уствновите " + osType + " версию для " + osName);
+    }
+
+    public static void task3() {
+        System.out.println("Задача3");
+        int deliveryDistance = 100;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance > 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет!");
+        }
     }
 }
--- src/com/lessons/Main.java
+++ src/com/lessons/Main.java
@@ -1,10 +1,89 @@
 package com.lessons;
 
 public class Main {
-    public static void main(String[] args) {
-        task1();
+    public static void printSeparator() {
+        System.out.println("------------------------------------");
     }
-    public static void task1() {
 
+    public static void printIssues(int issueCount) {
+        System.out.println(issueCount);
+    }
+
+    public static int sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
+
+    public static int calculateSum(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void printGreetings(String n) {
+        System.out.println(n);
+    }
+
+    public static String fullName(String f, String l) {
+        return f + " " + l;
+    }
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        task1();
+        training();
+
+    }
+
+    public static void task1() {
+        int[] issuesByMonth = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
+        printSeparator();
+        for (int i = 0; i < issuesByMonth.length; i++) {
+            printIssues(issuesByMonth[i]);
+            if ((i + 1) % 3 == 0) {
+                printSeparator();
+            }
+        }
+        printSeparator();
+        int total = sum(issuesByMonth);
+        printIssues(total);
+    }
+
+    public static void training() {
+        printSeparator();
+        int num1 = 6;
+        int num2 = 5;
+        int[] box1 = {1, 2, 3};
+        String firstName = "Konstantin";
+        String lastName = "Momani";
+        int sumNumbers = calculateSum(num1, num2);
+        int sumArrElem = calculateSum(box1);
+        printGreetings(firstName);
+        printGreetings(lastName);
+        int[] box2 = {4, 7, 9};
+        int sumBoxElem = calculateSum(box2);
+        System.out.println(sumNumbers);
+        System.out.println(sumArrElem);
+        System.out.println(sumBoxElem);
+        String fullName = fullName(firstName, lastName);
+        System.out.println(fullName);
+        printArr(box1);
+        printSeparator();
+        printArr(box2);
     }
 }
